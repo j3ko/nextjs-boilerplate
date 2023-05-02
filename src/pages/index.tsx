0a4e83037b36c95e6a3e3c123a18279473ba21b7
Jeffrey Ko <j3ko@users.noreply.github.com>
2023-05-02 09:27:57 +0700

    feat: 💄 add react-icons (#5)
    - https://github.com/react-icons/react-icons
    
    closes #1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google';
 import { Button, Stats } from 'react-daisyui';
+import { GiBearFace, GiBeet, GiSpaceship } from 'react-icons/gi';
 
 import useBoundStore from '@/store';
 
@@ -16,27 +17,47 @@ export default function Home() {
     <main className={`flex h-screen flex-col items-center space-y-4 p-24 ${inter.className}`}>
       <Stats className="shadow">
         <Stats.Stat>
+          <Stats.Stat.Item variant="figure" className="text-accent">
+            <GiBearFace className="inline-block h-8 w-8 stroke-current"></GiBearFace>
+          </Stats.Stat.Item>
           <Stats.Stat.Item variant="title">Total Bears</Stats.Stat.Item>
           <Stats.Stat.Item variant="value">{bears}</Stats.Stat.Item>
         </Stats.Stat>
       </Stats>
-      <Button className="w-64 rounded-full" color="primary" onClick={addBear}>
+      <Button
+        className="w-64 rounded-full"
+        color="primary"
+        onClick={addBear}
+        startIcon={<GiBearFace></GiBearFace>}>
         Add Bear
       </Button>
 
       <Stats className="shadow">
         <Stats.Stat>
+          <Stats.Stat.Item variant="figure" className="text-accent">
+            <GiBeet className="inline-block h-8 w-8 stroke-current"></GiBeet>
+          </Stats.Stat.Item>
           <Stats.Stat.Item variant="title">Total Beets</Stats.Stat.Item>
           <Stats.Stat.Item variant="value">{beets}</Stats.Stat.Item>
         </Stats.Stat>
       </Stats>
-      <Button className="w-64 rounded-full" color="primary" onClick={addBeet}>
+      <Button
+        className="w-64 rounded-full"
+        color="primary"
+        onClick={addBeet}
+        startIcon={<GiBeet></GiBeet>}>
         Add Beet
       </Button>
 
-      <Button className="flex w-64 flex-col rounded-full" color="primary" onClick={logout}>
-        <div>Battlestar Galactica</div>
-        <div className="text-[8px]">(aka. Reset)</div>
+      <Button
+        className="flex w-64 flex-col rounded-full"
+        color="primary"
+        startIcon={<GiSpaceship></GiSpaceship>}
+        onClick={logout}>
+        <div>
+          <div>Battlestar Galactica</div>
+          <div className="text-[8px]">(aka. Reset)</div>
+        </div>
       </Button>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    'node_modules/react-daisyui/dist/react-daisyui.cjs',
+    'node_modules/daisyui/dist/**/*',
   ],
   theme: {
     extend: {
